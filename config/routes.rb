4a4a7e743ce03dd00eac5bf2afdf805240659352
root <root@TL>
2024-04-30 02:37:18 +0200

    thrid commit

--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,11 @@ Rails.application.routes.draw do
     resources :gossips, only: [:index], controller: 'users'
   end
 
-  # Vous avez déjà défini la route pour l'index des gossips ci-dessus, donc cette ligne est redondante.
-  # get '/gossips', to: 'gossips#index'
+  resources :gossips, only: [:index, :show]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  get '/gossips', to: 'gossips#index'
 
   # Définit la route racine ("/")
   root 'static_pages#home'
